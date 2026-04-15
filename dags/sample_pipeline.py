7b9903e22a10ae2f8e38f6e1154907343debec3e
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2026-04-15 15:19:08 +0530

    modified:   dags/sample_pipeline.py

--- dags/sample_pipeline.py
+++ dags/sample_pipeline.py
@@ -1,55 +1,36 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from datetime import datetime, timedelta
+import pendulum
+from airflow.decorators import dag, task
 
 
-# Default arguments
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-}
-
-
-# Task functions
-def start_task():
-    print("Starting the pipeline...")
-
-
-def process_task():
-    print("Processing data...")
-
-
-def end_task():
-    print("Pipeline completed.")
-
-
-# DAG definition
-with DAG(
+@dag(
     dag_id='sample_pipeline',
-    default_args=default_args,
-    description='A simple sample DAG',
     schedule='@daily',
-    start_date=datetime(2024, 1, 1),
+    start_date=pendulum.datetime(2024, 1, 1, tz='UTC'),
     catchup=False,
     tags=['example'],
-) as dag:
+)
+def sample_pipeline():
 
-    start = PythonOperator(
-        task_id='start',
-        python_callable=start_task,
-    )
+    @task
+def start_task():
+        print("Starting the pipeline...")
 
-    process = PythonOperator(
-        task_id='process',
-        python_callable=process_task,
-    )
 
-    end = PythonOperator(
-        task_id='end',
-        python_callable=end_task,
-    )
+    @task
+def process_task():
+        print("Processing data...")
 
-    # Task dependencies
+
+    @task
+def end_task():
+        print("Pipeline completed.")
+
+
+    start = start_task()
+    process = process_task()
+    end = end_task()
+
     start >> process >> end
+
+
+dag = sample_pipeline()
