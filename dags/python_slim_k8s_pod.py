1c9445ceaa4a16d466c125a7d463bac423dd571c
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2026-04-21 13:35:59 +0530

    new file:   dags/python_slim_k8s_pod.py

---
+++ dags/python_slim_k8s_pod.py
@@ -0,0 +1,36 @@
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="python_slim_k8s_pod",
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+    tags=["kubernetes", "python-slim"],
+) as dag:
+
+    run_python = KubernetesPodOperator(
+        task_id="run_python_script",
+
+        name="python-slim-pod",
+        namespace="default",  # change if needed
+
+        image="python:3.14-slim",
+
+        cmds=["python", "-c"],
+        arguments=[
+            """
+import datetime
+print("Hello from python:slim container")
+print("Current time:", datetime.datetime.now())
+"""
+        ],
+
+        get_logs=True,
+        is_delete_operator_pod=True,
+
+        in_cluster=True,  # True if Airflow is running inside K8s
+    )
+
+    run_python
