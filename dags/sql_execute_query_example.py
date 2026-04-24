68b7d066a53740c74b36aa9bddcd28499d22be58
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2026-04-24 19:53:43 +0530

    modified:   dags/sql_execute_query_example.py

--- dags/sql_execute_query_example.py
+++ dags/sql_execute_query_example.py
@@ -13,7 +13,7 @@ from airflow.sdk import dag
 def sql_execute_query_example():
     query_airflow_metadata = SQLExecuteQueryOperator(
         task_id='query_airflow_metadata',
-        conn_id='postgres_default',
+        conn_id='my_postgres',
         sql='SELECT id, dag_id, logical_date, state FROM dag_run ORDER BY logical_date DESC LIMIT 10;',
     )
 
