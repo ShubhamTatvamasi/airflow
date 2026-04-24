7720b8f3163a7a51ef57ed5b19e3940582e48578
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2026-04-24 12:04:06 +0530

    new file:   dags/bash_operator_example.py

---
+++ dags/bash_operator_example.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timezone
+from airflow.operators.bash import BashOperator
+from airflow.sdk import dag
+
+@dag(
+    dag_id='bash_operator_example',
+    schedule='@daily',
+    start_date=datetime(2026, 3, 24, tzinfo=timezone.utc),
+    catchup=False,
+    tags=['example'],
+)
+
+def bash_operator_example():
+    print_date = BashOperator(
+        task_id='print_date',
+        bash_command='date',
+    )
+
+    sleep_task = BashOperator(
+        task_id='sleep_task',
+        bash_command='sleep 5',
+    )
+
+    completion = BashOperator(
+        task_id='completion',
+        bash_command='echo "BashOperator workflow complete"',
+    )
+
+    print_date >> sleep_task >> completion
+
+
+dag = bash_operator_example()
