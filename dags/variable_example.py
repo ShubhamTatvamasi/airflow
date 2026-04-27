83bb198eaeaf5fd870f40410a9d180dbf657d1fe
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2026-04-27 13:37:29 +0530

    new file:   dags/variable_example.py

---
+++ dags/variable_example.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timezone
+from airflow.sdk import dag, task
+from airflow.models import Variable
+
+@dag(
+    dag_id='variable_example',
+    schedule='@daily',
+    start_date=datetime(2026, 3, 23, tzinfo=timezone.utc),
+    catchup=False,
+    tags=['example', 'variable'],
+)
+def variable_example():
+
+    @task
+    def print_environment():
+        environment = Variable.get("environment")
+        print(f"ENVIRONMENT: {environment}")
+
+    print_environment()
+
+variable_example()
